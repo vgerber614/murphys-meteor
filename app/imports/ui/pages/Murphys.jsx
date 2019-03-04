c82acc55a12d151b3d2cd43f9c25819833dc8ba6
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2019-03-04 12:43:07 -1000

    Complete Murphys mockup

---
+++ app/imports/ui/pages/Murphys.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Middle from '../components/Middle';
+
+export default class Murphys extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <Middle/>
+        </div>
+    );
+  }
+}
